16e186bf1ead13dc3fd3026cd829e987fa23494d
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-23 11:49:25 -0300

    Function-Implementação da interface

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,8 +3,10 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
@@ -22,10 +24,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		double factor = 1.1;  //opcional
-					
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+		List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
 		
-		list.forEach(System.out::println);
+		names.forEach(System.out::println);
 	}
 }
