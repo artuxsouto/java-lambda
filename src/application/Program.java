de528935b114aaaae93d775865b36e0fbd63eb7b
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-22 21:19:40 -0300

    Predicate - Reference method com método não stático

--- src/application/Program.java
+++ src/application/Program.java
@@ -22,7 +22,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.removeIf(Product::staticProductPredicate);
+		list.removeIf(Product::nonStaticProductPredicate);
 		
 		for (Product i : list) {
 			System.out.println(i);
