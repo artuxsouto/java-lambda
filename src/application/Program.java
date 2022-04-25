8d1c064564d54fb0f8ae5e4d6c7e726cd3e1a709
Artur Siqueira <artursiqueirasouto@hotmail.com>
2022-04-25 13:32:39 -0300

    Função que recebe função como parâmetro

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,9 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import model.services.ProductService;
 
 public class Program {
 
@@ -23,9 +23,10 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
+		ProductService ps = new ProductService();
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
+		double sum = ps.filteredSum(list, p -> p.getName().charAt(0) == 'T');
 		
-		names.forEach(System.out::println);
+		System.out.println("Sum = " + String.format("%.2f", sum));
 	}
 }
